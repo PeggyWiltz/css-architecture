2cd9decfc690d5969ad63082fa73fd4a83ad2e9f
Peggy Wiltz <hengist2001@hotmail.com>
2015-06-23 22:37:25 -0700

    initial version of video player from HTML5 for masterminds book, to be modified

---
+++ js/player.js
@@ -0,0 +1,47 @@
+$(function() {
+
+  function init() {
+    maxim = 600;
+    mmedia = document.getElementById("media");
+    play = document.getElementById("play");
+    bar = document.getElementById("bar");
+    progress = document.getElementById("progress");
+
+    play.addEventListener("click", push, false);
+    bar.addEventListener("click", move, false);
+  }
+
+  function push() {
+      if (!mmedia.paused && !mmedia.ended) {
+        mmedia.pause();
+        play.innerHTML = "Play";
+        window.clearInterval(loop);
+      }
+      else {
+        mmedia.play();
+        play.innerHTML = "Pause";
+        loop = setInterval(status, 1000);
+      }
+  }
+
+  function status() {
+    if (!mmedia.ended) {
+      var size = parseInit(mmedia.currentTime * maxim / mmedia.duration);
+      progress.style.width = size + 'px';
+    }
+    else {
+      progress.style.width = '0px';
+      play.innerHTML = 'Play';
+      window.clearInterval(loop);
+    }
+  }
+
+  function move(e) {
+      if (!mmedia.paused && !mmedia.ended) {
+        var mouseX = e.pageX = bar.offsetLeft;
+        var newTime = mouseX * mmedia.duration / maxim;
+        mmedia.currentTime = newTime;
+        progress.style.width = mouseX + 'px';
+      }
+  }
+});
