9faf0e4ea155d16472130fb12378259f1e9bb9b7
Peggy Wiltz <hengist2001@hotmail.com>
2015-06-25 14:10:18 -0700

    Replacing javascript with jQuery now working for play and mute buttons.  Added styles.

--- js/player.js
+++ js/player.js
@@ -1,46 +1,62 @@
-$(function() {
+$(document).ready(function() {
 
-    maxim = 600;
-    mmedia = document.getElementById("media");
-    play = document.getElementById("play");
-    bar = document.getElementById("bar");
-    progress = document.getElementById("progress");
-
-    play.addEventListener("click", push, false);
-    bar.addEventListener("click", move, false);
+  $("#player__video").addClass('paused');
+  $("#player__video").addClass('unmuted');
   
-
-  function push() {
-      if (!mmedia.paused && !mmedia.ended) {
-        mmedia.pause();
-        play.innerHTML = "Play";
-        window.clearInterval(loop);
-      }
-      else {
-        mmedia.play();
-        play.innerHTML = "Pause";
-        loop = setInterval(status, 1000);
-      }
-  }
-
-  function status() {
-    if (!mmedia.ended) {
-      var size = parseInt(mmedia.currentTime * maxim / mmedia.duration);
-      progress.style.width = size + 'px';
+  $('#player__button--play').click(function() {
+    var player = $("#player__video");
+    var playBtn = $('#player__button--play');
+    
+    if (player.hasClass('paused')) {
+      player.removeClass('paused');
+      player[0].play();
+      player.addClass('playing');
+      playBtn.html('Pause');
+    } else {
+      player.removeClass('playing');
+      player[0].pause();
+      player.addClass('paused');
+      playBtn.html('Play');
     }
-    else {
-      progress.style.width = '0px';
-      play.innerHTML = 'Play';
-      window.clearInterval(loop);
+  });
+  
+   $('#player__button--mute').click(function() {
+     var player = $("#player__video");
+      var muteBtn = $('#player__button--mute');
+    
+    if (player.hasClass('muted')) {
+      player.removeClass('muted');
+      player.prop('muted', false); 
+      player.addClass('unmuted');
+      muteBtn.html('Mute');
+    } else {
+      player.removeClass('unmuted');
+      player.prop('muted', true); 
+      player.addClass('muted');
+      muteBtn.html('Unmute');
     }
-  }
+  });
+  
+   //***************************************************
+/*    Given a <video> element with an id of ‘v':
 
-  function move(e) {
-      if (!mmedia.paused && !mmedia.ended) {
-        var mouseX = e.pageX = bar.offsetLeft;
-        var newTime = mouseX * mmedia.duration / maxim;
-        mmedia.currentTime = newTime;
-        progress.style.width = mouseX + 'px';
-      }
-  }
+<progress id='p' max='100' value='0'><span>0</span>% played</progress>
+Use this JavaScript:
+
+var video = document.getElementById('v');
+var pBar = document.getElementById('p');
+video.addEventListener('timeupdate', function() {
+  var percent = Math.floor((100 / video.duration) * video.currentTime);
+  pBar.value = percent;
+  pBar.getElementsByTagName('span')[0].innerHTML = percent;
+}, false);
+The JavaScript uses the timeupdate event, which is constantly triggered as the video is being played. 
+The value is converted into a percentage value using the video’s duration 
+(full length) and currentTime (where in the video it currently is). */
+   //****************************************************
+  $("#player__video").prop('muted', true); //mute
+  
+  $("#player__video").prop('muted', false); //unmute
+  
+  
 });
